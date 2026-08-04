c88bd6258b5fdcb7922dbe86112d8c893cd0deac
evandenham-stack <evandenham@mis.school.nz>
2026-08-04 14:35:07 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+player.onChat("chicken", function (num1) {
+    for (let index = 0; index < 100; index++) {
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+    }
+})
+player.onChat("run", function () {
+	
+})
